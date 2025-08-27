1373fdbc04558130402239dc32c951db6caa9925
sam-chankyu-lee <sam.chankyu.lee@gmail.com>
2025-08-27 03:50:35 -0400

    Added flags to regfile
    Added ability to read and write to flag registers in regfile

--- rtl/regfile.v
+++ rtl/regfile.v
@@ -4,7 +4,8 @@ module regfile(
     input clk,
     input rst, 
     input writeReg,
-    input [1:0] writeFlag,
+    input [1:0] writeEn,
+    input writeFlag,
 
     input [3:0] rdReg1,
     output [7:0] rdData1,
@@ -14,6 +15,9 @@ module regfile(
     output [7:0] rdData2,
     output [7:0] rdData2Lo,
     
+    input [7:0] flagData,
+    output [7:0] rdFlag,
+
     input [3:0] wrReg,
     input [15:0] wrData
 );
@@ -27,20 +31,25 @@ module regfile(
     assign rdData2 = regs[rdReg2];
     assign rdData2Lo = regs[rdReg2+1];
 
+    assign rdFlag = flags;
+
     always @(posedge clk) begin
         if (rst) begin
             for (int i = 0; i < 11; i=i+1) begin
                 regs[i] <= 0;
             end
         end else begin
-            case(writeFlag)
+            case(writeEn)
                 1: regs[wrReg] <= wrData[7:0];
                 2: begin
                     regs[wrReg] <= wrData[15:8];
                     regs[wrReg+1] <= wrData[7:0];
                 end
                 default: begin end
-            endcase //case writeFlag
+            endcase //case writeEn
+            if (writeFlag) begin
+                flags <= flagData;
+            end
         end
     end
 
--- rtl/tb/tb_regfile.v
+++ rtl/tb/tb_regfile.v
@@ -4,7 +4,9 @@ module tb_regfile;
     reg clk;
     reg rst; 
     reg writeReg;
-    reg [1:0] writeFlag;
+    reg [1:0] writeEn;
+    reg writeFlag;
+
     reg [3:0] rdReg1;
     reg [3:0] rdReg2;
     reg [3:0] wrReg;
@@ -17,6 +19,9 @@ module tb_regfile;
     wire [7:0] rdData2;
     wire [7:0] rdData2Lo;
 
+    reg [7:0] flagData;
+    wire [7:0] rdFlag;
+
     always #1 clk = ~clk;
     integer values[21:0];
 
@@ -24,6 +29,7 @@ module tb_regfile;
         .clk(clk),
         .rst(rst), 
         .writeReg(writeReg),
+        .writeEn(writeEn),
         .writeFlag(writeFlag),
 
         .rdReg1(rdReg1),
@@ -33,6 +39,9 @@ module tb_regfile;
         .rdReg2(rdReg2),
         .rdData2(rdData2),
         .rdData2Lo(rdData2Lo),
+
+        .flagData(flagData),
+        .rdFlag(rdFlag),
         
         .wrReg(wrReg),
         .wrData(wrData)
@@ -42,11 +51,13 @@ module tb_regfile;
         clk <= 0;
         rst <= 1;
         writeReg <= 0;
+        writeEn <= 0;
         writeFlag <= 0;
         rdReg1 <= 0;
         rdReg2 <= 0;
         wrReg <= 0;
         wrData <= 0;
+        flagData <= 0;
     endfunction
 
     task reset_release();
@@ -56,12 +67,12 @@ module tb_regfile;
     task write_and_read();
         for (int i=0; i<11; i=i+1) begin
             #2 wrReg <= i;
-            writeFlag <= 1;
+            writeEn <= 1;
             wrData <= i;
         end
         
         #2 wrReg <= 0;
-        writeFlag <= 0;
+        writeEn <= 0;
         wrData <= 0;
 
         for (int i=0; i<9; i=i+2) begin
@@ -85,15 +96,24 @@ module tb_regfile;
                 $display("[%0d] reg%0d : %0d", j, i, values[j*11+i]);
             end 
         end
-            
+    endtask
 
-        
+    task flags();
+        writeFlag <= 1;
+        flagData <= 7'b1111000;
+        #2 writeFlag <= 0;
+        $display("Flag Expected: %0d | Flag Value: %0d", 7'b1111000, rdFlag);
+        #2 writeFlag <= 1;
+        flagData <= 7'b0000000;
+        #2 writeFlag <= 0;
+        $display("Flag Expected: %0d | Flag Value: %0d", 0, rdFlag);
     endtask
 
     initial begin
         init();
         reset_release();
         write_and_read();
+        flags();
         $finish;
     end 
 
